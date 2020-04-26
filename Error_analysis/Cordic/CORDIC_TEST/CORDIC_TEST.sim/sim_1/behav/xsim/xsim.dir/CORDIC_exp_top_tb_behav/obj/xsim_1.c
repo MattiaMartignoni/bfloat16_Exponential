f2f21b0952dc4c134c6b7c4f06db3afdabf60f83
Mattia <mattia.martignoni@outlook.it>
2020-04-27 00:16:52 +0200

    Added a folder with results of error analysis with all possible input bits for cordic module. Also made some modifications to tb file for error analysis to work with n of bits from 16 to 22

---
+++ Error_analysis/Cordic/CORDIC_TEST/CORDIC_TEST.sim/sim_1/behav/xsim/xsim.dir/CORDIC_exp_top_tb_behav/obj/xsim_1.c
@@ -0,0 +1,163 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+typedef void (*funcp)(char *, char *);
+extern int main(int, char**);
+extern void execute_33(char*, char *);
+extern void execute_34(char*, char *);
+extern void execute_35(char*, char *);
+extern void execute_179(char*, char *);
+extern void execute_180(char*, char *);
+extern void execute_181(char*, char *);
+extern void execute_182(char*, char *);
+extern void execute_183(char*, char *);
+extern void execute_3(char*, char *);
+extern void execute_4(char*, char *);
+extern void execute_5(char*, char *);
+extern void execute_104(char*, char *);
+extern void vlog_const_rhs_process_execute_0_fast_for_reg(char*, char*, char*);
+extern void execute_106(char*, char *);
+extern void execute_158(char*, char *);
+extern void execute_159(char*, char *);
+extern void execute_160(char*, char *);
+extern void execute_161(char*, char *);
+extern void execute_162(char*, char *);
+extern void execute_163(char*, char *);
+extern void execute_164(char*, char *);
+extern void execute_165(char*, char *);
+extern void execute_166(char*, char *);
+extern void execute_167(char*, char *);
+extern void execute_168(char*, char *);
+extern void execute_169(char*, char *);
+extern void execute_170(char*, char *);
+extern void execute_171(char*, char *);
+extern void execute_172(char*, char *);
+extern void execute_173(char*, char *);
+extern void execute_174(char*, char *);
+extern void execute_175(char*, char *);
+extern void execute_176(char*, char *);
+extern void execute_177(char*, char *);
+extern void execute_178(char*, char *);
+extern void execute_7(char*, char *);
+extern void execute_8(char*, char *);
+extern void execute_9(char*, char *);
+extern void execute_10(char*, char *);
+extern void execute_11(char*, char *);
+extern void execute_107(char*, char *);
+extern void execute_108(char*, char *);
+extern void execute_109(char*, char *);
+extern void execute_110(char*, char *);
+extern void execute_111(char*, char *);
+extern void execute_112(char*, char *);
+extern void execute_113(char*, char *);
+extern void execute_114(char*, char *);
+extern void execute_115(char*, char *);
+extern void execute_116(char*, char *);
+extern void execute_117(char*, char *);
+extern void execute_118(char*, char *);
+extern void execute_119(char*, char *);
+extern void execute_120(char*, char *);
+extern void execute_121(char*, char *);
+extern void execute_122(char*, char *);
+extern void execute_123(char*, char *);
+extern void execute_13(char*, char *);
+extern void execute_14(char*, char *);
+extern void execute_37(char*, char *);
+extern void execute_38(char*, char *);
+extern void execute_39(char*, char *);
+extern void execute_184(char*, char *);
+extern void execute_185(char*, char *);
+extern void execute_186(char*, char *);
+extern void execute_187(char*, char *);
+extern void execute_188(char*, char *);
+extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+extern void vlog_transfunc_eventcallback_2state(char*, char*, unsigned, unsigned, unsigned, char *);
+funcp funcTab[69] = {(funcp)execute_33, (funcp)execute_34, (funcp)execute_35, (funcp)execute_179, (funcp)execute_180, (funcp)execute_181, (funcp)execute_182, (funcp)execute_183, (funcp)execute_3, (funcp)execute_4, (funcp)execute_5, (funcp)execute_104, (funcp)vlog_const_rhs_process_execute_0_fast_for_reg, (funcp)execute_106, (funcp)execute_158, (funcp)execute_159, (funcp)execute_160, (funcp)execute_161, (funcp)execute_162, (funcp)execute_163, (funcp)execute_164, (funcp)execute_165, (funcp)execute_166, (funcp)execute_167, (funcp)execute_168, (funcp)execute_169, (funcp)execute_170, (funcp)execute_171, (funcp)execute_172, (funcp)execute_173, (funcp)execute_174, (funcp)execute_175, (funcp)execute_176, (funcp)execute_177, (funcp)execute_178, (funcp)execute_7, (funcp)execute_8, (funcp)execute_9, (funcp)execute_10, (funcp)execute_11, (funcp)execute_107, (funcp)execute_108, (funcp)execute_109, (funcp)execute_110, (funcp)execute_111, (funcp)execute_112, (funcp)execute_113, (funcp)execute_114, (funcp)execute_115, (funcp)execute_116, (funcp)execute_117, (funcp)execute_118, (funcp)execute_119, (funcp)execute_120, (funcp)execute_121, (funcp)execute_122, (funcp)execute_123, (funcp)execute_13, (funcp)execute_14, (funcp)execute_37, (funcp)execute_38, (funcp)execute_39, (funcp)execute_184, (funcp)execute_185, (funcp)execute_186, (funcp)execute_187, (funcp)execute_188, (funcp)vlog_transfunc_eventcallback, (funcp)vlog_transfunc_eventcallback_2state};
+const int NumRelocateId= 69;
+
+void relocate(char *dp)
+{
+	iki_relocate(dp, "xsim.dir/CORDIC_exp_top_tb_behav/xsim.reloc",  (void **)funcTab, 69);
+
+	/*Populate the transaction function pointer field in the whole net structure */
+}
+
+void sensitize(char *dp)
+{
+	iki_sensitize(dp, "xsim.dir/CORDIC_exp_top_tb_behav/xsim.reloc");
+}
+
+void simulate(char *dp)
+{
+	iki_schedule_processes_at_time_zero(dp, "xsim.dir/CORDIC_exp_top_tb_behav/xsim.reloc");
+	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
+	iki_execute_processes();
+
+	// Schedule resolution functions for the multiply driven Verilog nets that have strength
+	// Schedule transaction functions for the singly driven Verilog nets that have strength
+
+}
+#include "iki_bridge.h"
+void relocate(char *);
+
+void sensitize(char *);
+
+void simulate(char *);
+
+extern SYSTEMCLIB_IMP_DLLSPEC void local_register_implicit_channel(int, char*);
+extern void implicit_HDL_SCinstatiate();
+
+extern SYSTEMCLIB_IMP_DLLSPEC int xsim_argc_copy ;
+extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
+
+int main(int argc, char **argv)
+{
+    iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_sv_type_file_path_name("xsim.dir/CORDIC_exp_top_tb_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/CORDIC_exp_top_tb_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/CORDIC_exp_top_tb_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+     iki_set_rc_trial_count(100);
+    (void) design_handle;
+    return iki_simulate_design();
+}
--- Sources/Cordic_srcs/README.txt
+++ Sources/Cordic_srcs/README.txt
@@ -1,1 +1,2 @@
 to make different bit cordic module is just needed to change the pkg (and testbench)
+testbench for error analysis is good for 16 - 22 bits
